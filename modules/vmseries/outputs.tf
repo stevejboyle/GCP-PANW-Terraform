1dd5c54db58e055f2de9ebaadcef2cdeb1803502
stevejboyle <steve@sjbconsulting.ai>
2025-06-04 16:28:47 -0400

    Fixed TFVars

--- modules/vmseries/outputs.tf
+++
@@ -1,7 +0,0 @@
-
-output "instance_self_link" {
-  value = google_compute_instance.this.self_link
-}
-output "instance_network_ip" {
-  value = google_compute_instance.this.network_interface[0].network_ip
-}
